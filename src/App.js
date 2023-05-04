326e96ab0f407258c0cb914b5539dd57368ad146
judygab <ivditigabeskiria@gmail.com>
2023-05-04 15:19:10 -0400

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="left-column">
+        <div className="content">
+          <img src="/astronaut.png" alt="your-image-description-here" />
+        </div>
+      </div>
+      <div className="right-column">
+        <div className="form-container">
+          <h1>Subscribe to Our Newsletter 🚀</h1>
+          <p>The Tech Insider is your go-to newsletter for all the latest news in the world of technology, startups, and devtools.</p>
+          <form>
+            <label htmlFor="email">Email:</label>
+            <div className='input-container'>
+              <input type="email" id="email" name="email" />
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
